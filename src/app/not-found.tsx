d535afc33fa0806734b5459c991261ada495762d
Kabe-H <75508588+Kabe-H@users.noreply.github.com>
2024-11-05 07:55:19 -0300

    page 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,20 @@
-import { Typography, Container, Button } from '@mui/material';
-import Link from 'next/link';
+import { Typography, Container, Button } from "@mui/material";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <Container style={{ textAlign: 'center', marginTop: '150px', minHeight:"50vh" }}>
+    <Container
+      style={{ textAlign: "center", marginTop: "150px", minHeight: "50vh" }}
+    >
       <Typography variant="h1" component="h1" gutterBottom>
         404
       </Typography>
       <Typography variant="h6" component="h2" gutterBottom>
-        Oops! The page you're looking for doesn't exist.
+        ¡Ups! La página que estás buscando no existe.
       </Typography>
       <Link href="/" passHref>
         <Button variant="contained" color="primary">
-          Go back to Home
+          Volver
         </Button>
       </Link>
     </Container>
